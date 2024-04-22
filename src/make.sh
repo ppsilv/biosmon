89ab663c2db362c174528dd8a4196a542378b8c2
pdsilva <ppsilv@gmail.com>
2024-04-22 20:34:11 -0300

    Added a Makefile

--- src/make.sh
+++ src/make.sh
@@ -8,8 +8,9 @@ rm -Rf tmp/$i.*
 #Constroy o binario
 
 echo $i
-ca65 -I /usr/share/cc65/asminc -l $i.lst -D $i $i.s -o tmp/$i.o &&
-ld65 -vm -m tmp/$i.map -C $i.cfg tmp/$i.o -o tmp/$i.bin -Ln tmp/$i.lbl
+#ca65 -I /usr/share/cc65/asminc -l $i.lst -D $i $i.s -o tmp/$i.o &&
+ca65 -g -l tmp/$i.lst -D $i $i.s -o tmp/$i.o &&
+ld65 -C $i.cfg -vm  -m tmp/$i.map  -o tmp/$i.bin tmp/$i.o  -Ln tmp/$i.sym 
 
 # For ROM
 #bintomon -v -l 0xa000 -r 0xbd0d tmp/wozmon.bin > tmp/wozmon.mon
